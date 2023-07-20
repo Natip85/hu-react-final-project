cae47133e3c2e8820b35628bf4945a646924aea2
Nati Peretz <nati.peretz1985@gmail.com>
2023-07-20 14:57:46 +0300

    chnaged mongodb path

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db_conn: 'mongodb://localhost:27017/HackerU_BCard',
+    db_conn: 'mongodb://127.0.0.1:27017/HackerU_BCard',
     jwt_token: 'myPrivateKey',
     EMAIL_PASSWORD: 'crqftepjimrdmphd'
 }
